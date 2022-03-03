b1de468d21fd382149acac908757ec0211137f5f
chaiya1239900 <52321420+chaiya1239900@users.noreply.github.com>
2022-03-03 18:43:21 +0700

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,14 @@
+<!-- JavaScript  -->
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+<nav class="navbar navbar-ex-lg navbar-white bg-dark text-white">
+	<div class="container-fluid p-3">
+		<div class="mx-auto">
+			CopyRight &copy; 2020-2021 by <a href="admin/index.php">DMR</a> | All right reserved.
+			<a href="admin/index.html"></a>
+		</div>
+	</div>
+</nav>
+
+</html>
---
+++ include/headertop.php
@@ -0,0 +1,19 @@
+<div class="container shadow-lg my-3 p-3 rounded">
+	<div class="row">
+
+		<div class="col-md-1 my-20 ">
+			<div style="width: 10rem;">
+				<img src="images/services/thanos1.png" class="card-img-top rounded" alt="">
+			</div>
+		</div>
+
+		<div class="col-md-11">
+			<div style="width: 50rem;">
+				<h4 class="mt-4" style="margin-left: 7rem;">วิทยาลัยเทคนิคตราด</h4>
+				<div class="underline" style="margin-left: 7rem;"></div>
+				<h4 class="ml-auto" style="margin-left: 7rem;">Trat Techical College</h4>
+			</div>
+		</div>
+
+	</div>
+</div>
